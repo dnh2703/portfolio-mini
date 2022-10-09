0f7bc88a855c790799940f8d26b51d92c5fb94eb
dnh2703 <dnh2703@gmail.com>
2022-10-09 14:42:42 +0700

    update 1.5

--- js/main.js
+++ js/main.js
@@ -64,4 +64,3 @@ sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
 sr.reveal(`.profile__buttons`, {delay: 800})
 sr.reveal(`.filters__content`, {delay: 900})
 sr.reveal(`.filters`, {delay: 1000})
-1
